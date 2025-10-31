bce1406b9595a37010e4a613e4aaefb8907c14de
Alex Yu <alexyu.dc@gmail.com>
2025-10-31 17:50:39 +0800

    Initial Commit

---
+++ n24clock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  n24clock
+//
+//  Created by Alex Yu on 2025/10/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ n24clock/n24clockApp.swift
@@ -0,0 +1,17 @@
+//
+//  n24clockApp.swift
+//  n24clock
+//
+//  Created by Alex Yu on 2025/10/31.
+//
+
+import SwiftUI
+
+@main
+struct n24clockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
